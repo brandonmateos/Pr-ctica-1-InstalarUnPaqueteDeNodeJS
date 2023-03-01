1f4ec17bdb5292099179d71cb20ccc2eabf06a90
Brandon Lozano <blozano0@ucol.mx>
2023-03-01 08:03:49 -0600

    practica9

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+const express = require("express");
+const app = express();
 
-const express = require('express')
-const app = express()
+/*app.get('/', function (req, res) {
+  res.send('Hello World, this is the root route');
+});*/
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  //route handler  
+//Nota: inyectar código HTML en una respuesta
+  res.send(`
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Prueba de HTML</h1>
+    
+</body>
+</html>`);
+});
+
+/*app.get ('/prueba', (req, res) => {
+  res.send('Hello World, frome route Prueba');
+});*/
+
 //Puerto por defecto: 3000
-app.listen(3000)
+app.listen(3000);
 
 /*Notas:
 1. Para ejecutar el servidor, en la consola de comandos, nos situamos en la carpeta 
