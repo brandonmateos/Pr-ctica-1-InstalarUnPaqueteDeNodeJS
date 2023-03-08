a7dc9390e31bb741c07c0bcc1283bd78dfba0cdd
Brandon Lozano <blozano0@ucol.mx>
2023-03-08 09:00:50 -0600

    Practica12

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 var express = require("express");
 //declaramos una app de express
 var app = express();
-
 //creamos una carpeta virtual y la vamos a mapear a una carpeta real
 app.use("/assets", express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+
+
 app.use('/', function (req, res, next) {
   console.log('Request Url: ' + req.url);
   next();
@@ -14,38 +16,14 @@ app.use('/', function (req, res, next) {
 var port = process.env.PORT || 3000;
 
 //primera ruta a nivel raiz
-app.get("/", function (req, res) {res.send(`
-  <html><head><link href=assets/style.css type=text/css rel=stylesheet />
-    </head><body><h1>Hello World</h1></body></html>
-  `);
+app.get("/", function(req, res) {
+  res.render('index');
 });
 
-
-/*app.get('/', (req, res) => {
-  cad = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="/assets/style.css" rel="stylesheet">
-      <title>Document</title>
-  </head>
-  <body>
-      <h1>Hello word, this is the root route update </h1>
-  </body>
-  </html>
-  `
-  res.send(cad);
-});*/
 //ruta, recibe un parametro
 //para llamar en el local host http://localhost:3000/person/John
 app.get("/person/:id", function (req, res) {
-  res.send(
-    "<html><head></head><body><h1>Person: " +
-      req.params.id +
-      "</h1></body></html>"
-  );
+  res.render('person', { ID: req.params.id });
 });
 
 //ruta /api, regresa un objeto JSON
