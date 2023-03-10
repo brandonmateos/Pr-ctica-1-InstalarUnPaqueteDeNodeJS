a3dfcc9647e400e3708a03ab1ce0cc55bab5296e
Brandon Lozano <blozano0@ucol.mx>
2023-03-10 09:40:39 -0600

    Practica13

--- app.js
+++ app.js
@@ -15,15 +15,36 @@ app.use('/', function (req, res, next) {
 //setteamos el puerto
 var port = process.env.PORT || 3000;
 
+app.get('/index/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const isEven = id % 2 === 0;
+  const elements = Array.from({ length: id }, (_, index) => index + 1);
+
+  const data = {
+    number: id,
+    isEven,
+    elements,
+  };
+
+  res.render('index', data);
+});
+/*
 //primera ruta a nivel raiz
 app.get("/", function(req, res) {
+  //numero par o impar
   res.render('index');
 });
+*/
 
 //ruta, recibe un parametro
 //para llamar en el local host http://localhost:3000/person/John
-app.get("/person/:id", function (req, res) {
-  res.render('person', { ID: req.params.id });
+let data = [
+  { id: 1, nombre: "John", apellido: "Doe", edad: 25 },
+  { id: 2, nombre: "Jane", apellido: "Perez", edad: 23 },
+  { id: 3, nombre: "Mary", apellido: "Hernandez", edad: 21 },
+];
+app.get("/personas", function (req, res) {
+  res.render('personas', { data });
 });
 
 //ruta /api, regresa un objeto JSON
