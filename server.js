6ed0857a96e84127a65872803540f5ea4af29066
Brandon Lozano <blozano0@ucol.mx>
2023-03-24 08:18:01 -0600

    Practica15

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var port = process.env.PORT || 3000;
 //creamos una carpeta virtual y la vamos a mapear a una carpeta real
 app.use("/assets", express.static(__dirname + "/public"));
 
+//indicamos como vamos a parsear peticiones de URL(datos dentro del Body)
+app.use(express.urlencoded({ extended: false }));
+
 //utilizar ejs como motor de vistas
 app.set("view engine", "ejs");
 
@@ -20,9 +23,26 @@ app.get("/", function (req, res) {
 });
 
 //segunda ruta a nivel raiz
-app.get("/person/:id", (req, res) => {
-  res.render("person", { ID: req.params.id, MENSAGGE: req.query.menssage, TIME: req.query.time });
+app.get("/student", (req, res) => {
+  res.render("student");
 });
+
+//cuando lo pinta con el metodo send
+/*app.post('/addStudent', function (req, res) {
+  res.send(` Nombre: ${req.body.nombre} 
+  Edad: ${req.body.edad}
+  Nss: ${req.body.nss}
+  Tipo de sangre: ${req.body.tipoSangre}`)
+})*/
+//solo una de las rutas add student puede estar activ
+app.post('/addStudent', function (req, res) {
+  res.render('displayData', {
+    nombre: req.body.nombre,
+    edad: req.body.edad,
+    nss: req.body.nss,
+    tipoSangre: req.body.tipoSangre
+  })
+})
 
 //inicio del servidor
 app.listen(port);
