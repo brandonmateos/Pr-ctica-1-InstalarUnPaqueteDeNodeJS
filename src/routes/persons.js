c6c6efea506485cc3b74b1507ef3f2da9a664aec
Brandon Lozano <blozano0@ucol.mx>
2023-05-05 20:30:31 -0600

    Practica20

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -6,7 +6,31 @@ let Person = require('../models/persons');
 
 router.get('/gente', async (req, res) => {
     const Persons = await Person.find();
-    res.json(Persons);
+    res.render('persons', {Persons})
+});
+
+router.get('/addPerson',(req,res)=>{
+    res.render('addPerson');
+});
+
+router.post('/addPerson', (req, res) => {
+    /*
+        Este modelo contiene el Schema de MongoDB 
+        que permite crear un nuevo documento
+    */
+    const newPerson = Person({
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        tipoSangre: req.body.tipoSangre,
+        nss: req.body.nss
+    });
+
+    newPerson
+    //invocamos el metodo "save" para persistir
+    //el nuevo documento en la coleccion
+    .save()
+    .then((data) => {res.redirect('/gente')})
+    .catch((error) => {res.json({message: error})});
 });
 
 module.exports = router;
